c139e36280ab72a88562ab39579d125923c3e3cb
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-05-23 09:18:03 -0700

    added weapons

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ function createElement(elementType, src, left, bottom) {
 }
 function newImage() {
     let greenCharacter = createElement("img", "assets/green-character.gif", "100px", "100px");
-    let pineTree = createElement("img", "assets/pine-tree.png", "450px", "200px")
-
+    let pineTree = createElement("img", "assets/pine-tree.png", "450px", "200px");
+    let tree = createElement("img", "assets/tree.png", "200px", "300px");
+    let pillar = createElement("img", "assets/pillar.png", "350px", "100px");
+    let crate = createElement("img", "assets/crate.png", "150px", "200px");
+    let well = createElement("img", "assets/well.png", "500px", "425px");
 }
 newImage();
+
+function newItem() {
+    let sword = createElement("img", "assets/sword.png", "500px", "405px")
+    let shield = createElement("img", "assets/shield.png", "165px", "185px")
+    let staff = createElement("img", "assets/staff.png", "600px", "100px")
+    document.addEventListener("dblclick", function (event) {
+        if (event.target === sword || event.target === shield || event.target === staff) {
+            event.target.remove();
+        }
+    }
+    )
+}
+newItem();

7e3e15489d52a9a14ea185a8b19a95bc1a41be98
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-05-23 08:13:20 -0700

    created function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function createElement(elementType, src, left, bottom) {
+    let element = document.createElement(elementType);
+    element.src = src;
+    element.style.position = "fixed";
+    element.style.left = left;
+    element.style.bottom = bottom;
+    document.body.append(element);
+    return element;
+}
+function newImage() {
+    let greenCharacter = createElement("img", "assets/green-character.gif", "100px", "100px");
+    let pineTree = createElement("img", "assets/pine-tree.png", "450px", "200px")
+
+}
+newImage();
